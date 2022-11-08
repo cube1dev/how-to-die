028bee9453e7469ba7d8e2ae58ddd60198294742
Project_TL <me@projecttl.net>
2022-11-08 19:51:19 +0900

    feat: create intro complete!

---
+++ Assets/Scripts/TextFadeIn.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TextFadeIn : MonoBehaviour {
+    private Text text;
+    public Text before;
+    public float fadeTime = 1F;
+    public float delay = 0F;
+
+    private void Awake() {
+        text = GetComponent<Text>();
+        StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeIn() {
+        yield return new WaitForSeconds(delay);
+
+        Color color = text.color;
+        while (color.a < fadeTime) {
+            color.a += Time.deltaTime;
+            yield return 0;
+            text.color = color;
+        }
+    }
+}
